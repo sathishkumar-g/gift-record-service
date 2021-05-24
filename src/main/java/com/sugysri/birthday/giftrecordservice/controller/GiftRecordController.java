75641a639c9b871912867789306384243294605d
sathishkumar-g <50577411+sathishkumar-g@users.noreply.github.com>
2021-05-24 23:50:02 +0530

    Updating heroku url

--- src/main/java/com/sugysri/birthday/giftrecordservice/controller/GiftRecordController.java
+++ src/main/java/com/sugysri/birthday/giftrecordservice/controller/GiftRecordController.java
@@ -1,7 +1,5 @@
 package com.sugysri.birthday.giftrecordservice.controller;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
