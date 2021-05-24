5a4840422dac64c84f48c284027611ab4cff317e
sathishkumar-g <50577411+sathishkumar-g@users.noreply.github.com>
2021-05-25 01:50:13 +0530

    restore eureka client

--- src/main/java/com/sugysri/birthday/giftrecordservice/GiftRecordServiceApplication.java
+++ src/main/java/com/sugysri/birthday/giftrecordservice/GiftRecordServiceApplication.java
@@ -5,12 +5,12 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication()
-//@EnableEurekaClient
+@EnableEurekaClient
 //@EnableCaching
 public class GiftRecordServiceApplication {
 
