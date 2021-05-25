b80b89b90de515a23675e3effbcc1c3daf63427f
sathishkumar-g <50577411+sathishkumar-g@users.noreply.github.com>
2021-05-25 13:36:28 +0530

    Update GiftRecordServiceApplication.java

--- src/main/java/com/sugysri/birthday/giftrecordservice/GiftRecordServiceApplication.java
+++ src/main/java/com/sugysri/birthday/giftrecordservice/GiftRecordServiceApplication.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
