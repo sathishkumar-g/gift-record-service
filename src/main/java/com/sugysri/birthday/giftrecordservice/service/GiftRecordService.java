cf0ee15f8b3dc8864d85ce87d585530a23fb1ab3
sathishkumar-g <50577411+sathishkumar-g@users.noreply.github.com>
2021-06-03 18:23:20 +0530

    propr

--- src/main/java/com/sugysri/birthday/giftrecordservice/service/GiftRecordService.java
+++ src/main/java/com/sugysri/birthday/giftrecordservice/service/GiftRecordService.java
@@ -1,6 +1,7 @@
 package com.sugysri.birthday.giftrecordservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,37 +16,47 @@ public class GiftRecordService {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	
+	@Value("${gift.record.get.url}")
+	private String getUrl;
+
+	@Value("${gift.record.add.url}")
+	private String addUrl;
+
+	@Value("${gift.record.edit.url}")
+	private String updateUrl;
+
+	@Value("${gift.record.delete.url}")
+	private String deleteUrl;
+
+	@Value("${gift.record.delete.multiple.url}")
+	private String deleteMultipleUrl;
+
 	public GiftRecord getGiftRecord() {
 		GiftRecord giftRecord = null;
 		// giftRecord =
 		// restTemplate.getForObject("https://get-gift-record-service.herokuapp.com/gift/get",
 		// GiftRecord.class);
-		giftRecord = restTemplate.getForObject("https://get-gift-record-service.herokuapp.com/gift/get",
-				GiftRecord.class);
+		giftRecord = restTemplate.getForObject(getUrl, GiftRecord.class);
 		return giftRecord;
 	}
+
 	public GiftRecordItem editGiftRecord(GiftRecordItem giftRecordItem) {
 		// return
 		// restTemplate.getForObject("https://get-gift-record-service.herokuapp.com/gift/edit",
 		// GiftRecordItem.class);
-		return restTemplate.postForObject("https://get-gift-record-service.herokuapp.com/gift/edit", giftRecordItem,
-				GiftRecordItem.class);
+		return restTemplate.postForObject(updateUrl, giftRecordItem, GiftRecordItem.class);
 	}
 
 	public void deleteGiftRecord(GiftRecordItem giftRecordItem) {
-		restTemplate.postForObject("https://get-gift-record-service.herokuapp.com/gift/delete", giftRecordItem,
-				GiftRecordItem.class);
+		restTemplate.postForObject(deleteUrl, giftRecordItem, GiftRecordItem.class);
 	}
 
 	public void deleteGiftRecords(GiftRecord giftRecord) {
-		restTemplate.postForObject("https://get-gift-record-service.herokuapp.com/gift/selectiveDelete", giftRecord,
-				GiftRecordItem.class);
+		restTemplate.postForObject(deleteMultipleUrl, giftRecord, GiftRecordItem.class);
 	}
 
 	public GiftRecordItem addGiftRecord(GiftRecordItem giftRecordItem) {
-		return restTemplate.postForObject("https://get-gift-record-service.herokuapp.com/gift/add", giftRecordItem,
-				GiftRecordItem.class);
+		return restTemplate.postForObject(addUrl, giftRecordItem, GiftRecordItem.class);
 	}
 
 }
--- src/main/java/com/sugysri/birthday/giftrecordservice/service/UserService.java
+++ src/main/java/com/sugysri/birthday/giftrecordservice/service/UserService.java
@@ -1,6 +1,7 @@
 package com.sugysri.birthday.giftrecordservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,9 @@ public class UserService {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Value("${user.authenticate.url}")
+	private String authUrl;
+
 	public UserDetails getUserRecord(UserDetails userDetails) {
 		/*
 		 * userDetails = restTemplate.postForObject(
@@ -18,8 +22,7 @@ public class UserService {
 		 * userDetails, UserDetails.class);
 		 */
 
-		userDetails = restTemplate.postForObject("https://get-gift-record-service.herokuapp.com/user/authenticate",
-				userDetails, UserDetails.class);
+		userDetails = restTemplate.postForObject(authUrl, userDetails, UserDetails.class);
 		return userDetails;
 	}
 
